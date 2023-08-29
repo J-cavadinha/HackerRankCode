8b67efdc523f5769a967299e15d9aea89670e55b
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-08-29 17:44:02 -0300

    Create Nthterm.c

---
+++ C/Nthterm.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c, int counter) {
+    // n - numero de steps
+    if(counter == n){
+        return a;
+    } else {
+        return find_nth_term(n, a+b+c, c, b, counter+1);
+    }
+}
+
+int main() {
+    int n, a, b, c;
+    int counter = 3;
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c, counter);
+ 
+    printf("%d", ans); 
+    return 0;
+}
