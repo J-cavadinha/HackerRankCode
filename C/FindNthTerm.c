df7353058cca9a4869db19cf38c875f5cd3caf1d
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-08-30 23:15:25 -0300

    Update FindNthTerm.c

--- C/FindNthTerm.c
+++ C/FindNthTerm.c
@@ -8,33 +8,27 @@
 #include <stdlib.h>
 //Complete the following function.
 
-int find_nth_term(int n, int a, int b, int c, int counter) {
+int find_nth_term(int n, int a, int b, int c) {
     // n - numero de steps
-    if(n < 4){
-        if(n == 3){
+        if(n == 3)
             return c;
-        }
-        if(n == 2){
+        if(n == 2)
             return b;
-        }
-        if(n == 1){
+        if(n == 1)
             return a;
-        }
-    }
-    
-    if(counter == n){
-        return a;
-    } else {
-        return find_nth_term(n, a+b+c, c, b, counter+1);
-    }
+        else return find_nth_term(n-1, b, c, a+b+c);    // Obs final código
 }
+
 
 int main() {
     int n, a, b, c;
-    int counter = 3;
     scanf("%d %d %d %d", &n, &a, &b, &c);
-    int ans = find_nth_term(n, a, b, c, counter);
+    int ans = find_nth_term(n, a, b, c);
  
     printf("%d", ans); 
     return 0;
 }
+
+// Se n>3, vai executar a recursividade
+// n-1 diminui n até que tenha que retornar c
+// Já que vai retornar c, em vez de retornar (n-1, a+b+c, b, c), retorna-se a+b+c no lugar de c
